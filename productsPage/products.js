bdda9b839c2c1eb4524a33b23997da44713c4071
PhiliciaWatson <philiciawatson7@gmail.com>
2021-05-26 11:28:26 -0400

    idk

--- productsPage/products.js
+++ productsPage/products.js
@@ -1,29 +1,30 @@
+
 const products = [
     {
         title:'Product1',
         image:'../images/pedigree.png',
-        price: 20.99,
+        price: 18.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product2',
         image:'../images/purplefood.png',
-        price: 20.99,
+        price: 23.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product3',
         image:'../images/pedigree2.png',
-        price: 20.99,
+        price: 12.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product4',
         image:'../images/dogchow.png',
-        price: 20.99,
+        price: 10.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
@@ -37,63 +38,63 @@ const products = [
     {
         title:'Product6',
         image:'../images/purinaCat.png',
-        price: 20.99,
+        price: 26.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product7',
         image:'../images/catComplete.png',
-        price: 20.99,
+        price: 30.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product8',
         image:'../images/kitCat.png',
-        price: 20.99,
+        price: 40.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product9',
         image:'../images/Nutrirve.png',
-        price: 20.99,
+        price: 24.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product10',
         image:'../images/Rio.png',
-        price: 20.99,
+        price: 21.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product11',
         image:'../images/tretra.png',
-        price: 20.99,
+        price: 22.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product12',
         image:'../images/chichlide.png',
-        price: 20.99,
+        price: 26.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product13',
         image:'../images/Crispy.png',
-        price: 20.99,
+        price: 27.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
     {
         title:'Product14',
         image:'../images/Atrelexa.png',
-        price: 20.99,
+        price: 25.99,
         description:'lcekkmewkoxmqwkomsowmsko2wmdkosowmkdmkodwmkowmd'
 
     },
@@ -101,7 +102,20 @@ const products = [
 ]
 
 
-var postHTML = "";
+
+
+    // future challenge sort method
+    // function Low(e){
+
+    //     return postHTML += products.sort(function(a, b) {
+    //         return parseFloat(a.price) - parseFloat(b.price);
+    //     });
+    // }
+
+    
+
+
+var postHTML = " ";
 
 for(var i = 0; i < products.length;i++){
 
@@ -116,3 +130,4 @@ for(var i = 0; i < products.length;i++){
 }
 
 document.getElementById('products').innerHTML = postHTML;
+
